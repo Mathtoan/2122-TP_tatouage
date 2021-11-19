24c5addf2fc015e1da228a1f3f01c8242f9672ec
celialecpe <celia.le@cpe.fr>
2021-11-19 18:24:08 +0100

    Ajout degradations

--- main.py
+++ main.py
@@ -1,23 +1,41 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import cv2
+import argparse
 import os
+
+import cv2
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy
+import scipy.misc
+import skimage.io
+import skimage.transform as sktf
+from matplotlib import cm
+from PIL import Image
+
+# Parser
+parser = argparse.ArgumentParser(description='Insertion et detection tatouage')
+parser.add_argument('-d','--display', action='store_true',
+                    help='Display the image')
+parser.add_argument('-f','--filename',default='cameraman.tif',
+                    help='Filename of the image on which to apply the tattoo')
+parser.add_argument('-k',default=1024,
+                    help='Value of K, the number of points in the tattoo')
+parser.add_argument('-a','--alpha',default=0.1,
+                    help='Value of alpha, the weighting value of the tattoo')
+parser.add_argument('-t','--degradation_type', default='gaussian',
+                    choices=['gaussian', 'compression', 'translation', 'rotation', 'zoom'],
+                    help='Choose the type of degradation to apply to the tattooed image')
+
+
+args = parser.parse_args()
 
 dir_figure = './figure'
 
 if not os.path.exists(dir_figure):
     os.makedirs(dir_figure)
 
-# Lecture de l'image
-I = plt.imread('cameraman.tif')
-I_fft2 = np.fft.fft2(I)
-I_fftshift = np.fft.fftshift(I_fft2)
 
-# Generation du motif de tatouage
-T = np.random.randn(1024)
-alpha = 0.05
-
-def insertion_tattoo(I,T,alpha):
+def insertion_tattoo(I,T,alpha,display=True):
     """
     Input :
         I : image a tatouer
@@ -62,31 +80,31 @@ def insertion_tattoo(I,T,alpha):
     I_tattooed = np.fft.ifft2(I_tattooed_fft2)
 
     # Affichage
+    if display:
+        plt.figure()
+        plt.subplot(131)
+        plt.imshow(I, 'gray')
+        plt.title('Image originale')
+        plt.subplot(132)
+        plt.imshow(np.log(np.abs(I_tattooed_fft2)), 'gray')
+        plt.title('TF de l image')
+        plt.subplot(133)
+        plt.imshow(np.log(np.abs(I_tattooed_fft2_shift)), 'gray')
+        plt.title('TF shift de l image')
+        plt.savefig(os.path.join(dir_figure, 'image avec fft.png'))
+        plt.show()
+        plt.close()
 
-    plt.figure()
-    plt.subplot(131)
-    plt.imshow(I, 'gray')
-    plt.title('Image originale')
-    plt.subplot(132)
-    plt.imshow(np.abs(np.log(I_tattooed_fft2)), 'gray')
-    plt.title('TF de l image')
-    plt.subplot(133)
-    plt.imshow(np.abs(np.log(I_tattooed_fft2_shift)), 'gray')
-    plt.title('TF shift de l image')
-    plt.savefig(os.path.join(dir_figure, 'image avec fft.png'))
-    # plt.show()
-    plt.close()
-
-    plt.figure()
-    plt.subplot(121)
-    plt.imshow(I, 'gray')
-    plt.title('Image originale')
-    plt.subplot(122)
-    plt.imshow(np.abs(I_tattooed), 'gray')
-    plt.title('Image tatouee')
-    plt.savefig(os.path.join(dir_figure, 'image vs tatoue.png'))
-    # plt.show()
-    plt.close()
+        plt.figure()
+        plt.subplot(121)
+        plt.imshow(I, 'gray')
+        plt.title('Image originale')
+        plt.subplot(122)
+        plt.imshow(np.abs(I_tattooed), 'gray')
+        plt.title('Image tatouee')
+        plt.savefig(os.path.join(dir_figure, 'image vs tatoue.png'))
+        plt.show()
+        plt.close()
 
     return I_tattooed
 
@@ -111,33 +129,25 @@ def PSNR(I, I_tattooed):
 
     return psnr
 
-# alphas = np.linspace(0, 1, 21)
-# alphas[0] = alphas[0]+0.01
-# psnr = []
-# for k in alphas:
-#     I_tattooed = insertion_tattoo(I,T,k)
-#     psnr.append(PSNR(I, I_tattooed))
+def affichage_PSNR(I,T):
+    alphas = np.linspace(0, 1, 21)
+    alphas[0] = alphas[0]+0.01
+    psnr = []
+    for k in alphas:
+        I_tattooed = insertion_tattoo(I, T, k, display=False)
+        psnr.append(PSNR(I, I_tattooed))
 
-# plt.figure(dpi=200)
-# plt.plot(alphas, psnr)
-# plt.grid()
-# plt.xlabel('alpha')
-# plt.ylabel('PSNR')
-# plt.title('Evolution du PSNR en fonction de alpha')
-# plt.savefig(os.path.join(dir_figure, 'PSNR.png'))
-# plt.close()
-
-# plt.figure()
-# plt.subplot(131)
-# plt.imshow(I, 'gray')
-# plt.title('Image')
-# plt.subplot(132)
-# plt.imshow(np.log(np.abs(I_fft2)), 'gray')
-# plt.title('FFT2')
-# plt.subplot(133)
-# plt.imshow(I_tattooed.real, 'gray')
-# plt.title('FFT shift')
-# plt.show()
+    plt.figure(dpi=200)
+    plt.plot(alphas, psnr)
+    plt.grid()
+    plt.xlabel('alpha')
+    plt.ylabel('PSNR')
+    plt.title('Evolution du PSNR en fonction de alpha')
+    plt.savefig(os.path.join(dir_figure, 'PSNR.png'))
+    plt.show()
+    plt.close()
+    
+    return
 
 def detection_tattoo(I, I_tattooed, T, alpha, display=True):
     """
@@ -149,9 +159,9 @@ def detection_tattoo(I, I_tattooed, T, alpha, display=True):
         display : affichage des tatouages (True par defaut)
     Output :
         detected_tattoo : Indique si le tatouage est detecte
-        match_tattoo : Indique si le tatoauge correspond au tatouage original
+        match_tattoo : Indique si le tatouage correspond au tatouage original
         
-    Detecte la presence ou non du tatouage, et si oui, calcul coefficient de correlation
+    Detecte la presence ou non du tatouage, et si oui, calcule le coefficient de correlation
     pour determiner si le tatouage correspond au tatouage original.
     """
 
@@ -187,7 +197,7 @@ def detection_tattoo(I, I_tattooed, T, alpha, display=True):
         plt.imshow(np.abs(T_real), 'gray')
         plt.title('Tatouage reel')
         plt.savefig(os.path.join(dir_figure, 'tatouage estime vs reel.png'))
-        # plt.show()
+        plt.show()
         plt.close()
 
     # Vectorisation des tatouage, utile pour les calculs
@@ -197,7 +207,7 @@ def detection_tattoo(I, I_tattooed, T, alpha, display=True):
 
     # Detection du tatouage
     T_est_norm = np.linalg.norm(T_est_vect)
-    print(T_est_norm)
+    print("Norme: ", T_est_norm)
     if T_est_norm < 1 :
         print('Aucun tatouage dectecte')
         return detected_tattoo, match_tattoo
@@ -209,16 +219,13 @@ def detection_tattoo(I, I_tattooed, T, alpha, display=True):
         mean_T_est = np.mean(T_est_vect)
         mean_T_real = np.mean(T_real_vect)
         threshold = .5
-        sum1 = 0
-        sum2 = 0
-        sum3 = 0
-        for i in range(N):
-            sum1 += (T_est_vect[i] - mean_T_est)*(T_real_vect[i]-mean_T_real)
-            sum2 += (T_est_vect[i] - mean_T_est)**2
-            sum3 += (T_real_vect[i] - mean_T_real)**2
-        
-        gamma = sum1/np.sqrt(sum2*sum3)
-        print('Coefficient de correlation :', gamma.real)
+
+        num = np.sum((T_est_vect-mean_T_est) * (T_real_vect-mean_T_real))
+        den = np.sqrt(np.sum((T_est_vect-mean_T_est)**2) * np.sum((T_real_vect-mean_T_real)**2))
+        gamma = num / den
+
+        # print('num:',num,'den:',den)
+        print('Coefficient de correlation:', gamma.real)
 
         if gamma<threshold :
             print('Le tatouage detecte ne correspond pas au tatouage de base')
@@ -228,28 +235,69 @@ def detection_tattoo(I, I_tattooed, T, alpha, display=True):
             print('Le tatouage detecte correspond au tatouage de base')
             return detected_tattoo, match_tattoo
 
+def degradation(I_tattooed, type, display=True):
+    if type == 'gaussian':
+        mean = 0
+        std = 0.001 ** 0.5
+        gaussian_noise = np.random.normal(mean, std, I_tattooed.shape)*255
+        I_tattooed_degradee = I_tattooed.real+gaussian_noise
+
+        print(np.amax(I_tattooed.real), np.amin(I_tattooed.real))
+    
+    if type == 'compression':
+        compressed_file = 'compressed_image.jpeg'
+        quality = 50
+        skimage.io.imsave(compressed_file, I_tattooed.real, quality=quality)
+        I_tattooed_degradee = plt.imread(compressed_file)
+
+    if type == 'translation':
+        t=10
+        axis=0
+        I_tattooed_degradee = np.roll(I_tattooed, t, axis)
+
+    if type == 'rotation':
+        theta=10
+        I_tattooed_degradee = sktf.rotate(I_tattooed.real, angle=theta, mode='constant', cval=0.0, resize=False)
+
+    if type == 'zoom':
+        n = 10
+        I_tattooed_degradee = sktf.resize(I_tattooed.real[n:-n, n:-n], I_tattooed.shape, anti_aliasing=False)
+
+    if display:
+        plt.figure()
+        plt.subplot(121)
+        plt.imshow(I_tattooed.real, 'gray')
+        plt.title('Image tatouee')
+        plt.subplot(122)
+        plt.imshow(I_tattooed_degradee.real, 'gray')
+        plt.title('Image degradee')
+        plt.savefig(os.path.join(dir_figure, 'image tatouee vs image tatouee degradee par '+type+'.png'))
+        plt.show()
+        plt.close()
+
+    return I_tattooed_degradee
+
+# Lecture de l'image
+filename=args.filename
+I = plt.imread(filename)
+I_fft2 = np.fft.fft2(I)
+I_fftshift = np.fft.fftshift(I_fft2)
+
+# Generation du motif de tatouage
+K=args.k
+T = np.random.randn(K)
+
 # Detection de tatouage
-alpha = 0.1
-I_tattooed = insertion_tattoo(I, T, alpha)
-print(detection_tattoo(I, I_tattooed, T, alpha, display=False))
+alpha = args.alpha
+I_tattooed = insertion_tattoo(I, T, alpha, display=args.display)
+if args.display: affichage_PSNR(I,T)
+print(detection_tattoo(I, I_tattooed, T, alpha, display=args.display))
 
 # Degradation du tatouage
+type = args.degradation_type
+I_tattooed_degradee = degradation(I_tattooed,type, display=args.display)
 
-gaussian_noise = np.random.normal(0, 0.001 ** 0.5, I_tattooed.shape)*255
+
+print(detection_tattoo(I, I_tattooed_degradee, T, alpha, display=args.display))
 
 
-I_tattooed_gauss = I_tattooed.real+gaussian_noise
-
-print(np.amax(I_tattooed.real), np.amin(I_tattooed.real))
-
-plt.figure()
-plt.subplot(121)
-plt.imshow(I_tattooed.real, 'gray')
-plt.title('Image tatouee')
-plt.subplot(122)
-plt.imshow(I_tattooed_gauss.real, 'gray')
-plt.title('Image degradee')
-plt.show()
-plt.close()
-
-print(detection_tattoo(I, I_tattooed_gauss, T, alpha))
